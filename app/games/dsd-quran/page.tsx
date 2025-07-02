2e1b7144e6b70d3f7be094f1e67fb86c8d8f9855
Rafique <69503725+iciso@users.noreply.github.com>
2025-07-02 21:02:50 +0530

    Update page.tsx
    changed route.

--- app/games/dsd-quran/page.tsx
+++ app/games/dsd-quran/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 // app/games/dsd-quran/page.tsx
-import DSDQuranGame from "@/components/games/dsd-quran/DSDQuranGame";
+import DSDQuranGame from "@/games/dsd-quran/DSDQuranGame";
 
 export default function DSDQuranGamePage() {
   return <DSDQuranGame />;
