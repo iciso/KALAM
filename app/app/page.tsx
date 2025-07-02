b0e6f300fc49cabb64b1befdfc3b297a4e2fc8bf
Rafique <69503725+iciso@users.noreply.github.com>
2025-07-02 22:53:41 +0530

    Update page.tsx

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,1 +1,12 @@
+"use client";
 
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My v0 Project</h1>
+      <Link href="/games/dsd-quran">Play DSD Quran Game</Link>
+    </div>
+  );
+}
