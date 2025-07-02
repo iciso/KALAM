194de413f1b76683e27cb73c78bcd2786cb8d041
Rafique <69503725+iciso@users.noreply.github.com>
2025-07-02 20:59:42 +0530

    Update page.tsx
    route corrected

--- app/App/page.tsx
+++ app/App/page.tsx
@@ -1,20 +1,16 @@
 "use client"
 
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import DSDQuranGame from './components/games/dsd-quran/DSDQuranGame';
-import Games from './pages/Games';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DSDQuranGame from "@/components/games/dsd-quran/DSDQuranGame";
 
-const App: React.FC = () => {
+export default function Home() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/games" element={<Games />} />
         <Route path="/games/dsd-quran" element={<DSDQuranGame />} />
-        {/* Add other game routes here */}
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
