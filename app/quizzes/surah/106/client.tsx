a59db7467147c95363a33c39d0701fbada0b8135
Rafique <icisoi.club@gmail.com>
2025-08-03 15:07:12 +0530

    Update client.tsx
    Syntax error

--- app/quizzes/surah/106/client.tsx
+++ app/quizzes/surah/106/client.tsx
@@ -6,6 +6,6 @@ import dynamic from "next/dynamic"
 // Use dynamic import to avoid hydration issues with client components
 const SurahQuiz = dynamic(() => import("@/components/surah-quiz"), { ssr: false })
 
-export default function AlFalaqQuizClient() {
+export default function AlQurayshQuizClient() {
   return <SurahQuiz quizData={alQurayshQuizData} />
 }
