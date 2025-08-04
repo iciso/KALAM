cc19c11b07b58e4b80dbe1e5abff55d0044f8618
Rafique <icisoi.club@gmail.com>
2025-08-04 11:40:31 +0530

    Create client.tsx
    Creating Surah 1 client

---
+++ app/quizzes/surah/1/client.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { alFatihahQuizData } from "@/data/surah-1-quiz-data"
+import dynamic from "next/dynamic"
+
+// Use dynamic import to avoid hydration issues with client components
+const SurahQuiz = dynamic(() => import("@/components/surah-quiz"), { ssr: false })
+
+export default function AlFatihahQuizClient() {
+  return <SurahQuiz quizData={alFatihahQuizData} />
+}
