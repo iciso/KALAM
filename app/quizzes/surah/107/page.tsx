186b9a1a29906d39ac689299abfe999e16cd31b0
Rafique <icisoi.club@gmail.com>
2025-08-10 09:32:06 +0530

    Update page.tsx with Meta data for 107 Surah
    Update page.tsx with Meta data for 107 Surah

--- app/quizzes/surah/107/page.tsx
+++ app/quizzes/surah/107/page.tsx
@@ -1,5 +1,10 @@
 import AlMaunQuizClient from "./client"
 
+export const metadata = {
+  title: "Surah Al-Maun Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Maun (The Alms Giving)",
+}
+
 export default function AlMaunQuizPage() {
   return <AlMaunQuizClient />
 }
