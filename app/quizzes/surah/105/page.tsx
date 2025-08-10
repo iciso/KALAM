48a55d886ba99c188c081ccad01a11e17dd1af9f
Rafique <icisoi.club@gmail.com>
2025-08-10 11:18:55 +0530

    Create page.tsx for Al-Fil Surah 105
    Create page.tsx for Al-Fil Surah 105

---
+++ app/quizzes/surah/105/page.tsx
@@ -0,0 +1,10 @@
+import AlFilQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Al-Fil Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Fil (The Elephant)",
+}
+
+export default function AlFilQuizPage() {
+  return <AlFilQuizClient />;
+}
