6643d5992467ae148162db6e876d2e5aa169bdb1
Rafique <icisoi.club@gmail.com>
2025-08-10 16:44:04 +0530

    Create page.tsx for Surah 104, Al-Humazah, The Slanderer
    Create page.tsx for Surah 104, Al-Humazah, The Slanderer

---
+++ app/quizzes/surah/104/page.tsx
@@ -0,0 +1,10 @@
+import AlHumazahQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Al-Humazah Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Humazah (The Slanderer)",
+}
+
+export default function AlHumazahQuizPage() {
+  return <AlHumazahQuizClient />;
+}
