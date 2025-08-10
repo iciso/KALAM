ce2cfd574ec5a96e93b08120645b728fd3f73c43
Rafique <icisoi.club@gmail.com>
2025-08-10 09:45:41 +0530

    Update page.tsx with Metadata for 113 Surah
    Update page.tsx with Metadata for 113 Surah

--- app/quizzes/surah/113/page.tsx
+++ app/quizzes/surah/113/page.tsx
@@ -1,5 +1,10 @@
 import AlFalaqQuizClient from "./client"
 
+export const metadata = {
+  title: "Surah Al-Falaq Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Falaq (The Daybreak)",
+}
+
 export default function AlFalaqQuizPage() {
   return <AlFalaqQuizClient />
 }
