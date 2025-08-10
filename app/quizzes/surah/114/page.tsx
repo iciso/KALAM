bb9e388ead599d30dc23890b791b8b2c36fc4bb5
Rafique <icisoi.club@gmail.com>
2025-08-10 09:51:33 +0530

    Update page.tsx with Metadata for 114
    Update page.tsx with Metadata for 114

--- app/quizzes/surah/114/page.tsx
+++ app/quizzes/surah/114/page.tsx
@@ -1,5 +1,10 @@
 import AlNasQuizClient from "./client"
 
+export const metadata = {
+  title: "Surah An-Nas Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah An-Nas (The Mankind)",
+}
+
 export default function AlNasQuizPage() {
   return <AlNasQuizClient />
 }
