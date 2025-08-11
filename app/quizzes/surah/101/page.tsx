543fd3755385543f443d606a7ff5492ba23cac91
Rafique <icisoi.club@gmail.com>
2025-08-11 20:35:02 +0530

    Create page.tsx for Surah 101 Al-Qariah
    Create page.tsx for Surah 101 Al-Qariah

---
+++ app/quizzes/surah/101/page.tsx
@@ -0,0 +1,10 @@
+import AlQariahQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Al-Qariah Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Qariah (The Calamity)",
+}
+
+export default function AlQariahQuizPage() {
+  return <AlQariahQuizClient />;
+}
