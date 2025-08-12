19a5768455302d6d166513b3ff3e3e19ff3b4433
Rafique <icisoi.club@gmail.com>
2025-08-12 09:00:24 +0530

    Create page.tsx for Surah 100 Al-Adiyat
    Create page.tsx for Surah 100 Al-Adiyat

---
+++ app/quizzes/surah/100/page.tsx
@@ -0,0 +1,10 @@
+import AlAdiyatQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Al-Adiyat Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Adiyat (The Chargers)",
+}
+
+export default function AlAdiyatQuizPage() {
+  return <AtTakathurQuizClient />;
+}
