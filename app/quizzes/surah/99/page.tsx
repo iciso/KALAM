142996a84738d15f57f29710eb3c8c5f97592b84
Rafique <icisoi.club@gmail.com>
2025-08-12 11:22:29 +0530

    Create page.tsx for Surah 99 Al-Zalzalah
    Create page.tsx for Surah 99 Al-Zalzalah

---
+++ app/quizzes/surah/99/page.tsx
@@ -0,0 +1,10 @@
+import AlZalzalahQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Al-Zalzalah Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Zalzalah (The Earthquake)",
+}
+
+export default function AlZalzalahQuizPage() {
+  return <AlZalzalahQuizClient />;
+}
