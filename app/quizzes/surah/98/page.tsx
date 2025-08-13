7b1d0be0cd95774acb949ce52f2a88393b8e90bb
Rafique <icisoi.club@gmail.com>
2025-08-13 09:34:43 +0530

    Create page.tsx for Surah No. 98, Al-Bayyinah
    Create page.tsx for Surah No. 98, Al-Bayyinah

---
+++ app/quizzes/surah/98/page.tsx
@@ -0,0 +1,10 @@
+import AlBayyinahQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Al-Bayyinah Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Bayyinah (The Evidence or The Clear Proof)",
+}
+
+export default function AlBayyinahQuizPage() {
+  return <AlZalzalahQuizClient />;
+}
