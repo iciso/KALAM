5f32243071b31bd83d77f1683d059c6916dee1ef
Rafique <icisoi.club@gmail.com>
2025-08-14 20:07:21 +0530

    Create page.tsx for Surah 96 Al-Alaq
    Create page.tsx for Surah 96 Al-Alaq

---
+++ app/quizzes/surah/96/page.tsx
@@ -0,0 +1,10 @@
+import AlAlaqQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Al-Alaq Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Alaq (The Embryo)",
+}
+
+export default function AlAlaqQuizPage() {
+  return <AlAlaqQuizClient />;
+}
