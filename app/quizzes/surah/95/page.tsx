0e74a1356e146085724b40315d1fb6e73021df8a
Rafique <icisoi.club@gmail.com>
2025-08-28 19:44:45 +0530

    Create page.tsx for Surah 95 At-Tin
    Create page.tsx for Surah 95 At-Tin

---
+++ app/quizzes/surah/95/page.tsx
@@ -0,0 +1,10 @@
+import AtTinQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah At-Tin Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah At-Tin (The Fig)",
+}
+
+export default function AtTinQuizPage() {
+  return <AtTinQuizClient />;
+}
