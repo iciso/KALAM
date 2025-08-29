17ef26292f1d763995a166332c45f6b9d3ed0432
Rafique <icisoi.club@gmail.com>
2025-08-29 20:20:54 +0530

    Create page.tsx for Surah 94 Ash - Sharh
    Create page.tsx for Surah 94 Ash - Sharh

---
+++ app/quizzes/surah/94/page.tsx
@@ -0,0 +1,10 @@
+import AshSharhQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Ash-Sharh Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Ash-Sharh (The Relief)",
+}
+
+export default function AshSharhQuizPage() {
+  return <AshSharhQuizClient />;
+}
