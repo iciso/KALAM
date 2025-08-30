65e4fe2a1eefb1206410406c0b194b8db51ce485
Rafique <icisoi.club@gmail.com>
2025-08-30 20:00:05 +0530

    Update loading.tsx

--- app/quizzes/surah/93/loading.tsx
+++ app/quizzes/surah/93/loading.tsx
@@ -1,4 +1,4 @@
-import { Skeleton } from "@/components/ui/skeleton";
+import { Skeleton } from "@/components/ui/skeleton"; 
 export default function Loading() {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 p-4">
