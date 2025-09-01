5d216c5e8f41b55c46ca692543dea00ef3822ad3
Rafique <icisoi.club@gmail.com>
2025-09-01 13:47:04 +0530

    Create page.tsx for Surah 92 Al-Lail The Night
    Create page.tsx for Surah 92 Al-Lail The Night

---
+++ app/quizzes/surah/92/page.tsx
@@ -0,0 +1,10 @@
+import AlLailQuizClient from "./client"; 
+
+export const metadata = {
+  title: "Surah Al-Lail Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Lail (The Night)",
+}
+
+export default function AlLailQuizPage() {
+  return <AlLailQuizClient />;
+}
