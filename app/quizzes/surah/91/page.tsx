e25b9770a69756f953f602fe4d871aaf274b72e6
Rafique <icisoi.club@gmail.com>
2025-09-07 13:13:46 +0530

    Create page.tsx Surah 91 Ash-Shams The Sun
    Create page.tsx Surah 91 Ash-Shams The Sun

---
+++ app/quizzes/surah/91/page.tsx
@@ -0,0 +1,10 @@
+import AshShamsQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah ASh-Shams Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Ash-Shams (The Sun)",
+}
+
+export default function AshShamsQuizPage() {
+  return <AshShamsQuizClient />;
+}
