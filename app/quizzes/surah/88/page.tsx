b13ce1c5286b77619c8cbdfca0f640b7405deac6
Rafique <icisoi.club@gmail.com>
2025-09-08 21:08:02 +0530

    Create page.tsx for Surah 88 Al-Ghashiyah The Overwhelming
    Create page.tsx for Surah 88 Al-Ghashiyah The Overwhelming

---
+++ app/quizzes/surah/88/page.tsx
@@ -0,0 +1,10 @@
+import AlGhashiyahQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Al-Ghashiyah Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Ghashiyah (The Overwhelming)",
+}
+
+export default function AlGhashiyahQuizPage() {
+  return <AlGhashiyahQuizClient />;
+}
