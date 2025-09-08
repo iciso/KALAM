0c458e6d5eb2b51b48263255230a887bd3256195
Rafique <icisoi.club@gmail.com>
2025-09-08 20:58:57 +0530

    Update page.tsx Typo in Metadata corrected
    Update page.tsx Typo in Metadata corrected

--- app/quizzes/surah/91/page.tsx
+++ app/quizzes/surah/91/page.tsx
@@ -1,7 +1,7 @@
 import AshShamsQuizClient from "./client";
 
 export const metadata = {
-  title: "Surah ASh-Shams Quiz | KALAM",
+  title: "Surah Ash-Shams Quiz | KALAM",
   description: "Test your knowledge of vocabulary from Surah Ash-Shams (The Sun)",
 }
 
