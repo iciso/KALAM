8f8297a4e50e16973fb0c0a98718cce7caebae25
Rafique <icisoi.club@gmail.com>
2025-09-08 13:18:19 +0530

    Create page.tsx for Surah 89 Al-Fajar The Dawn
    Create page.tsx for Surah 89 Al-Fajar The Dawn

---
+++ app/quizzes/surah/89/page.tsx
@@ -0,0 +1,10 @@
+import AlFajrQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Al-Fajr Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Fajr (The Dawn)",
+}
+
+export default function AlFajrQuizPage() {
+  return <AlFajrQuizClient />;
+}
