0b2e1fa385f4a3181a3043ea9f8d65e43c4497d4
Rafique <icisoi.club@gmail.com>
2025-09-13 21:05:29 +0530

    Create page.tsx

---
+++ app/quizzes/surah/91/page.tsx
@@ -0,0 +1,10 @@
+import AshShamsQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Ash-Shams Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Ash-Shams (The Sun)",
+}
+
+export default function AshShamsQuizPage() {
+  return <AshShamsQuizClient />;
+}
