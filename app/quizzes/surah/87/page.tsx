adea31757952618fd71cad5c251079d6cbd4400e
Rafique <icisoi.club@gmail.com>
2025-09-13 21:17:22 +0530

    Create page.tsx

---
+++ app/quizzes/surah/87/page.tsx
@@ -0,0 +1,10 @@
+import AlAlaQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Al-A'la Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-A'la (The Most High)",
+}
+
+export default function AlAlaQuizPage() {
+  return <AlAlaQuizClient />;
+}
