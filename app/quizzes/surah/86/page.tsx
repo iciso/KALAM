a4a74b2f0b531992b651aa01825c2f94c5698077
Rafique <icisoi.club@gmail.com>
2025-09-16 19:41:36 +0530

    Create page.tsx Surah 86 At-Tariq
    Create page.tsx Surah 86 At-Tariq

---
+++ app/quizzes/surah/86/page.tsx
@@ -0,0 +1,10 @@
+import AtTariqQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah At-Tariq Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah At-Tariq (The Knocking Visitor)",
+}
+
+export default function AtTariqQuizPage() {
+  return <AtTariqQuizClient />;
+}
