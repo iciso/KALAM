f057bbf76e8ddc23e4417a2aa62a6dbd47db0023
Rafique <icisoi.club@gmail.com>
2025-09-17 19:35:59 +0530

    Create page.tsx Surah 85 Al-Buruj The Constellations
    Create page.tsx Surah 85 Al-Buruj The Constellations

---
+++ app/quizzes/surah/85/page.tsx
@@ -0,0 +1,10 @@
+import AlBurujQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Al-Buruj Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Buruj (The Constellations)",
+}
+
+export default function AlBurujQuizPage() {
+  return <AlBurujQuizClient />;
+}
