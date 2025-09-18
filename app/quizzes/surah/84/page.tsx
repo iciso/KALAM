9d4f8ae6a4c434c17360f83c135598973167f760
Rafique <icisoi.club@gmail.com>
2025-09-18 19:42:11 +0530

    Create page.tsx Surah 84 Al-Inshiqaq The Splitting Asunder
    Create page.tsx Surah 84 Al-Inshiqaq The Splitting Asunder

---
+++ app/quizzes/surah/84/page.tsx
@@ -0,0 +1,10 @@
+import AlInshiqaqQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Al-Inshiqaq Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Inshiqaq (The Splitting Asunder)",
+}
+
+export default function AlInshiqaqQuizPage() {
+  return <AlInshiqaqQuizClient />;
+}
