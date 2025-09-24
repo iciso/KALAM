bb41fed5d15ab2c43ad9eedf06e98f524dff5052
Rafique <icisoi.club@gmail.com>
2025-09-24 19:38:14 +0530

    Create page.tsx Surah 83 Al-Mutaffifin The Defrauders
    Create page.tsx Surah 83 Al-Mutaffifin The Defrauders

---
+++ app/quizzes/surah/83/page.tsx
@@ -0,0 +1,10 @@
+import AlMutaffifinQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Al-Mutaffifin Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Mutaffifin (The Defrauders)",
+}
+
+export default function AlMutaffifinQuizPage() {
+  return <AlMutaffifinQuizClient />;
+}
