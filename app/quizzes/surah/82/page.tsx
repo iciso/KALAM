e5086a7032c1db84ca33b000410987d6db1069f3
Rafique <icisoi.club@gmail.com>
2025-09-25 19:43:32 +0530

    Create page.tsx Surah 82 Al-Infitar The Splitting Open
    Create page.tsx Surah 82 Al-Infitar The Splitting Open

---
+++ app/quizzes/surah/82/page.tsx
@@ -0,0 +1,10 @@
+import AlInfitarQuizClient from "./client";
+ 
+export const metadata = {
+  title: "Surah Al-Infitar Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Al-Infitar (The Splitting Open)",
+}
+
+export default function AlInfitarQuizPage() {
+  return <AlInfitarQuizClient />;
+}
