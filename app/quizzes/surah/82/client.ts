153e3e44e5b1970e96638d60a3215b13476cce1b
Rafique <icisoi.club@gmail.com>
2025-09-25 19:41:05 +0530

    Create client.ts Surah 82 Al-Infitar The Splitting Open
    Create client.ts Surah 82 Al-Infitar The Splitting Open

---
+++ app/quizzes/surah/82/client.ts
@@ -0,0 +1,7 @@
+"use client";
+import { alInfitarQuizData } from "@/data/surah-82-quiz-data";
+import dynamic from "next/dynamic";
+const SurahQuiz = dynamic(() => import("@/components/surah-quiz"), { ssr: false });
+export default function AlInfitarQuiz() {
+  return <SurahQuiz quizData={alInfitarQuizData} />;
+}
