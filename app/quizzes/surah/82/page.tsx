c35f723b2594d317492a5a95622f26221610d150
Rafique <icisoi.club@gmail.com>
2025-09-25 19:49:20 +0530

    Update page.tsx

--- app/quizzes/surah/82/page.tsx
+++ app/quizzes/surah/82/page.tsx
@@ -1,5 +1,5 @@
 import AlInfitarQuizClient from "./client";
- 
+
 export const metadata = {
   title: "Surah Al-Infitar Quiz | KALAM",
   description: "Test your knowledge of vocabulary from Surah Al-Infitar (The Splitting Open)",
