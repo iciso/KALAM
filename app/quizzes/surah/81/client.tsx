f2f7875e441b9a849f4f6ec570fb10bff29318ae
Rafique <icisoi.club@gmail.com>
2025-09-26 09:00:36 +0530

    Create client.tsx Surah 81 At-Takwir The Folding Up
    Create client.tsx Surah 81 At-Takwir The Folding Up

---
+++ app/quizzes/surah/81/client.tsx
@@ -0,0 +1,7 @@
+"use client"; 
+import { atTakwirQuizData } from "@/data/surah-81-quiz-data";
+import dynamic from "next/dynamic";
+const SurahQuiz = dynamic(() => import("@/components/surah-quiz"), { ssr: false });
+export default function AtTakwirQuiz() {
+  return <SurahQuiz quizData={atTakwirQuizData} />;
+} 
