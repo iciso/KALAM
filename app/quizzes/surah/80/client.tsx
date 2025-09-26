1041e2780adaabff0e1abf1dc2678723cfdb7d25
Rafique <icisoi.club@gmail.com>
2025-09-26 19:22:27 +0530

    Create client.tsx Surah No. 80 Abasa He frowned
    Create client.tsx Surah No. 80 Abasa He frowned

---
+++ app/quizzes/surah/80/client.tsx
@@ -0,0 +1,7 @@
+"use client"; 
+import { abasaQuizData } from "@/data/surah-80-quiz-data";
+import dynamic from "next/dynamic";
+const SurahQuiz = dynamic(() => import("@/components/surah-quiz"), { ssr: false });
+export default function AbasaQuiz() {
+  return <SurahQuiz quizData={abasaQuizData} />;
+} 
