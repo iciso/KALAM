144d3c26d1750a60cf4badcad09a8e27264b2d05
Rafique <icisoi.club@gmail.com>
2025-09-26 19:46:37 +0530

    Create page.tsx Surah 80 Abasa He Frowned
    Create page.tsx Surah 80 Abasa He Frowned

---
+++ app/quizzes/surah/80/page.tsx
@@ -0,0 +1,10 @@
+import AbasaQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Abasa Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah Abasa (He Frowned)",
+}
+
+export default function AbasaQuizPage() {
+  return <AbasaQuizClient />;
+}
