d731030986c050431c4fdef23c03bc8691c13a93
Rafique <icisoi.club@gmail.com>
2025-09-26 09:05:32 +0530

    Create page.tsxSurah 81 At Takwir The Folding Up
    Create page.tsxSurah 81 At Takwir The Folding Up

---
+++ app/quizzes/surah/81/page.tsx
@@ -0,0 +1,10 @@
+import AtTakwirQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah At-Takwir Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah At-Takwir (The Folding Up)",
+}
+
+export default function AtTakwirQuizPage() {
+  return <AtTakwirQuizClient />;
+}
