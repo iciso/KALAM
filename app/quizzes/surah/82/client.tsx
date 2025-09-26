b3a03b18d40ca045c498faeb16800f0670d532db
Rafique <icisoi.club@gmail.com>
2025-09-26 08:57:01 +0530

    Update client.tsx

--- app/quizzes/surah/82/client.tsx
+++ app/quizzes/surah/82/client.tsx
@@ -1,4 +1,4 @@
-"use client"; 
+"use client";
 import { alInfitarQuizData } from "@/data/surah-82-quiz-data";
 import dynamic from "next/dynamic";
 const SurahQuiz = dynamic(() => import("@/components/surah-quiz"), { ssr: false });
