98a9c060d3179ddbb60fba83a17ad32cab69cf50
Rafique <icisoi.club@gmail.com>
2025-09-27 13:53:55 +0530

    Create page.tsx Surah 79 An-Naziat Those Who Drag Forth
    Create page.tsx Surah 79 An-Naziat Those Who Drag Forth

---
+++ app/quizzes/surah/79/page.tsx
@@ -0,0 +1,10 @@
+import AnNaziatQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah An-Naziat Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah An-Naziat (Those Who Drag Forth)",
+}
+
+export default function AnNaziatQuizPage() {
+  return <AnNaziatQuizClient />;
+}
