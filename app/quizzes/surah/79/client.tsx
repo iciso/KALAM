6ef4dc64cef417ee28396c4efc3ff9a3b4bc1c91
Rafique <icisoi.club@gmail.com>
2025-09-27 13:50:40 +0530

    Create client.tsx Surah79 An-Naziat Those who Drag Forth
    Create client.tsx Surah79 An-Naziat Those who Drag Forth

---
+++ app/quizzes/surah/79/client.tsx
@@ -0,0 +1,7 @@
+"use client"; 
+import { anNaziatQuizData } from "@/data/surah-79-quiz-data";
+import dynamic from "next/dynamic";
+const SurahQuiz = dynamic(() => import("@/components/surah-quiz"), { ssr: false });
+export default function AnNaziatQuiz() {
+  return <SurahQuiz quizData={anNaziatQuizData} />;
+} 
