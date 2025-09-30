d38fa59935bc5a40bb3957066c9c003c7fc4917a
Rafique <icisoi.club@gmail.com>
2025-09-30 09:11:15 +0530

    Create client.tsx Surah 78 An-Naba The Great News
    Create client.tsx Surah 78 An-Naba The Great News

---
+++ app/quizzes/surah/78/client.tsx
@@ -0,0 +1,7 @@
+"use client"; 
+import { anNabaQuizData } from "@/data/surah-78-quiz-data";
+import dynamic from "next/dynamic";
+const SurahQuiz = dynamic(() => import("@/components/surah-quiz"), { ssr: false });
+export default function AnNabaQuiz() {
+  return <SurahQuiz quizData={anNabaQuizData} />;
+} 
