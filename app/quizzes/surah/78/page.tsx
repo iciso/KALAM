3f1440aa8e47acb1233ef8c9140485c7e3e7f106
Rafique <icisoi.club@gmail.com>
2025-10-01 08:54:20 +0530

    Create page.tsx Surah 78 An-Naba The Great News
    Create page.tsx Surah 78 An-Naba The Great News

---
+++ app/quizzes/surah/78/page.tsx
@@ -0,0 +1,10 @@
+import AnNabaQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah An-Naba Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah An-Naba (The Great News)",
+}
+
+export default function AnNabaQuizPage() {
+  return <AnNabaQuizClient />;
+}
