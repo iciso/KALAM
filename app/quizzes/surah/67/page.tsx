d444fd06d6069b8b7ea9cdeb388403a1d0f0d717
Rafique <icisoi.club@gmail.com>
2025-10-01 19:53:14 +0530

    Update page.tsx Syntax correction

--- app/quizzes/surah/67/page.tsx
+++ app/quizzes/surah/67/page.tsx
@@ -2,9 +2,9 @@ import AlMulkQuizClient from "./client";
 
 export const metadata = {
   title: "Surah Al-Mulk Quiz | KALAM",
-  description: "Test your knowledge of vocabulary from Surah An-Naba (The Sovereignty)",
+  description: "Test your knowledge of vocabulary from Surah Al-Mulk (The Sovereignty)",
 }
 
 export default function AlMulkQuizPage() {
-  return <AnNabaQuizClient />;
+  return <AlMulkQuizClient />;
 }
