1412b1024e8bbcf427df4b0e4c4e87dae3d5b6d0
Rafique <icisoi.club@gmail.com>
2025-10-01 19:47:44 +0530

    Create client.tsx Surah 67 Al-Mulk The Sovereignty
    Create client.tsx Surah 67 Al-Mulk The Sovereignty

---
+++ app/quizzes/surah/67/client.tsx
@@ -0,0 +1,7 @@
+"use client"; 
+import { alMulkQuizData } from "@/data/surah-67-quiz-data";
+import dynamic from "next/dynamic";
+const SurahQuiz = dynamic(() => import("@/components/surah-quiz"), { ssr: false });
+export default function AlMulkQuiz() {
+  return <SurahQuiz quizData={alMulkQuizData} />;
+} 
