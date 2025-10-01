c0c01d7ac99c18cdfa83cc34cea66251ad74d867
Rafique <icisoi.club@gmail.com>
2025-10-01 19:50:59 +0530

    Create page.tsx Surah 67 Al-Mulk The Sovereignty
    Create page.tsx Surah 67 Al-Mulk The Sovereignty

---
+++ app/quizzes/surah/67/page.tsx
@@ -0,0 +1,10 @@
+import AlMulkQuizClient from "./client";
+
+export const metadata = {
+  title: "Surah Al-Mulk Quiz | KALAM",
+  description: "Test your knowledge of vocabulary from Surah An-Naba (The Sovereignty)",
+}
+
+export default function AlMulkQuizPage() {
+  return <AnNabaQuizClient />;
+}
