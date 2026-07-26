d466cf652088587c8b84c101f52a68c00abcdc16
Rafique <icisoi.club@gmail.com>
2026-07-26 18:55:59 +0530

    Fix formatting issues in client.tsx

--- app/quizzes/surah/55/client.tsx
+++ app/quizzes/surah/55/client.tsx
@@ -1,4 +1,4 @@
-"use client"; 
+"use client";
 import { arRahmanQuizData } from "@/data/surah-55-quiz-data";
 import dynamic from "next/dynamic";
 const SurahQuiz = dynamic(() => import("@/components/surah-quiz"), { ssr: false });
