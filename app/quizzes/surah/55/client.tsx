fc19d52df89f29b239af33a3517305d0791f18df
Rafique <icisoi.club@gmail.com>
2026-07-26 20:28:44 +0530

    Rename arRahmanQuiz function to arRahmanQuizClient

--- app/quizzes/surah/55/client.tsx
+++ app/quizzes/surah/55/client.tsx
@@ -2,6 +2,6 @@
 import { arRahmanQuizData } from "@/data/surah-55-quiz-data";
 import dynamic from "next/dynamic";
 const SurahQuiz = dynamic(() => import("@/components/surah-quiz"), { ssr: false });
-export default function arRahmanQuiz() {
+export default function arRahmanQuizClient() {
   return <SurahQuiz quizData={arRahmanQuizData} />;
 }
